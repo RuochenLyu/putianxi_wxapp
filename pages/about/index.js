59b954b6163c7b7399ef1aa52fc22f14ce5746cb
Ruochen Lyu <kshift.lv@gmail.com>
2018-11-19 14:41:30 +0800

    ADD: 原文链接

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,4 +1,17 @@
 Page({
+  copyUrl() {
+    wx.setClipboardData({
+      data: 'https://cn.nytimes.com/china/20181116/china-private-hospitals-putian/',
+      success() {
+        wx.showToast({
+          title: '已复制当前文章链接',
+          icon: 'none',
+          duration: 2000
+        })
+      }
+    })
+  },
+
   onShareAppMessage() {
     return {
       title: '阅读「笼罩在丑闻中的医疗帝国：莆田系医院发迹史」'
